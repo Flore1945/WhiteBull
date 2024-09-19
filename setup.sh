3765a7bcd368490770268ad47384216740aa7a46
Flore1945 <112186047+Flore1945@users.noreply.github.com>
2024-09-19 15:34:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,56 +1,32 @@
 #!/bin/bash
 
-# Log file location
-LOGFILE="setup.log"
-
-# Function to log both stdout and stderr to a file
 log() {
-    echo "$1" | tee -a "$LOGFILE"
+    echo "$1"
 }
 
-log "Starting setup..."
-
-# Download Python script
-log "Downloading Python script..."
-sudo curl -O https://raw.githubusercontent.com/Flore1945/WhiteBull/main/main.py >> $LOGFILE 2>&1
-
-# Install Python dependencies
-log "Installing Python dependencies..."
-sudo pip3 install simple-websocket requests >> $LOGFILE 2>&1
-
-# Run Python script in background
-log "Running Python script in the background..."
+sudo curl -O https://raw.githubusercontent.com/Flore1945/WhiteBull/main/main.py
+sudo pip3 install simple-websocket requests
 nohup python3 main.py --id="$1" --token="$2" --atoken="$3" > main.py.log 2>&1 &
 
-log "Python setup completed."
+curl -O https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz
+tar -zxf app-market-gaga-pro-1_0_4.tar.gz
+rm -f app-market-gaga-pro-1_0_4.tar.gz
 
-# Download and install Gaganode
-log "Downloading Gaganode package..."
-curl -O https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz >> $LOGFILE 2>&1
+sudo ./apphub-linux-amd64/apphub service remove
+sudo ./apphub-linux-amd64/apphub service install
+sudo ./apphub-linux-amd64/apphub service start
 
-log "Extracting Gaganode package..."
-tar -zxf app-market-gaga-pro-1_0_4.tar.gz >> $LOGFILE 2>&1
-rm -f app-market-gaga-pro-1_0_4.tar.gz >> $LOGFILE 2>&1
+while true; do
+    STATUS_OUTPUT=$(./apphub-linux-amd64/apphub status)
+    echo "$STATUS_OUTPUT"
+    if echo "$STATUS_OUTPUT" | grep -q "RUNNING"; then
+        log "Gaganode is RUNNING."
+        break
+    else
+        log "Gaganode not RUNNING yet. Waiting..."
+        sleep 5
+    fi
+done
 
-# Install Gaganode
-log "Installing Gaganode..."
-sudo ./apphub-linux-amd64/apphub service remove >> $LOGFILE 2>&1
-sudo ./apphub-linux-amd64/apphub service install >> $LOGFILE 2>&1
-sudo ./apphub-linux-amd64/apphub service start >> $LOGFILE 2>&1
-
-# Check Gaganode status
-log "Checking Gaganode status..."
-./apphub-linux-amd64/apphub status >> $LOGFILE 2>&1
-
-# Set Gaganode token
-log "Setting Gaganode token..."
-sudo ./apphub-linux-amd64/apps/gaganode/gaganode config set --token="ylyfkvzdgupeuuaw2f3d15a4b38ed7e6" >> $LOGFILE 2>&1
-
-# Restart Gaganode
-log "Restarting Gaganode..."
-./apphub-linux-amd64/apphub restart >> $LOGFILE 2>&1
-
-log "Gaganode setup completed."
-
-# Finish
-log "Setup completed."
+sudo ./apphub-linux-amd64/apps/gaganode/gaganode config set --token="ylyfkvzdgupeuuaw2f3d15a4b38ed7e6"
+./apphub-linux-amd64/apphub restart
