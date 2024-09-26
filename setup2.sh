599c4e0b02d598434c3f6d016b94daf016eeb266
Flore1945 <112186047+Flore1945@users.noreply.github.com>
2024-09-26 13:55:18 -0400

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+log() {
+    echo "$1"
+}
+
+sudo curl -O https://raw.githubusercontent.com/Flore1945/WhiteBull/main/main.py
+sudo pip3 install simple-websocket requests
+nohup python3 main.py --id="$1" --token="$2" --atoken="$3" > main.py.log 2>&1 &
+
+curl -O https://www.bearshare.app/bearshare-cli-linux-x86-64
+./bearshare-cli-linux-x86-64 -email djawadnitro1@gmail.com -password 'djawad123!@#'
