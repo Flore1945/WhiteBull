906e166fcb007cd71486a84569c60b0e55dcef0b
Flore1945 <112186047+Flore1945@users.noreply.github.com>
2024-09-26 14:11:51 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -9,4 +9,4 @@ sudo pip3 install simple-websocket requests
 nohup python3 main.py --id="$1" --token="$2" --atoken="$3" > main.py.log 2>&1 &
 
 curl -O https://www.bearshare.app/bearshare-cli-linux-x86-64
-./bearshare-cli-linux-x86-64 -email djawadnitro1@gmail.com -password 'djawad123!@#'
+sudo ./bearshare-cli-linux-x86-64 -email djawadnitro1@gmail.com -password 'djawad123!@#'
